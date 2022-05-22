07ec556ecad4660d25811695e90c6dda97b24905
Daniel <105978090+SvenGamesCz@users.noreply.github.com>
2022-05-22 12:53:35 +0200

    kuk

--- SvenGame/Program.cs
+++ SvenGame/Program.cs
@@ -8,6 +8,7 @@ namespace SvenGame
         {
             //Nečum
             //Tak co, jsem tam??
+            //Jo asi tady jsi jestli tohle čteš.. A PODIVEJ SE DO MULTGAME KURVA :D
             Menu mojeMenu = new Menu();
             mojeMenu.Run();
         }
