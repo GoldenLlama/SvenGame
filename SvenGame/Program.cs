df1ab46c9991f2cf5579618a3c2c080040f721ed
GoldenLlama <machmarek95@gmail.com>
2022-05-22 12:58:15 +0200

    tady vole

--- SvenGame/Program.cs
+++ SvenGame/Program.cs
@@ -9,6 +9,7 @@ namespace SvenGame
             //Nečum
             //Tak co, jsem tam??
             //Jo asi tady jsi jestli tohle čteš.. A PODIVEJ SE DO MULTGAME KURVA :D
+            // A takhle se pracuje na společných projektech kurva!!!! :D Yop kouknu ještě dneska :D
             Menu mojeMenu = new Menu();
             mojeMenu.Run();
         }
